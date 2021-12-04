147ff461d9c95ac10fe3f75fbf5e9444d1619142
John <megaman5249@gmail.com>
2021-12-04 18:35:39 -0500

    Add undo last move functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const gameBoard = function(){
+const gameBoard = (function(){
     'use strict';
     function checkWin(){
         if (board[0]==board[1] && board[0]==board[2]){
@@ -13,6 +13,10 @@ const gameBoard = function(){
             return _getWinner(board[8]);
         }else if(board[2]==board[4] && board[2]==board[6]){
             return _getWinner(board[2]);
+        }else if(board[3]==board[4] && board[3]==board[5]){
+            return _getWinner(board[3]);
+        }else if(board[1]==board[4] && board[1]==board[7]){
+            return _getWinner(board[1]);
         }
     };
     function _getWinner(token){
@@ -22,48 +26,65 @@ const gameBoard = function(){
     function whosTurn(){
         return (this.turn%2==0)?players.player2:players.player1
     };
+    function undoLastMove(){
+        this.board[this.lastMove] = "";
+        this.turn --;
+        this.lastMove = null;
+    };
     let turn = 1;
+    let lastMove;
     let board = [
-        "0","1","2",
-        "3","4","5",
-        "6","7","8",
+        "1","2","3",
+        "4","5","6",
+        "7","8","9"
     ];
-    return {board, turn, checkWin, whosTurn};
-}();
+    return {board, turn, lastMove, checkWin, whosTurn, undoLastMove};
+})();
 
 
-const displayController = function(){
+const displayController = (function(){
     let grid = document.getElementById("game-board")
-    console.log(grid);
-
     for(i=0; i<gameBoard.board.length; i++){
         let element = document.createElement("div");
-        element.dataKey = i;
+        element["data-key"] = i;
         element.classList.add("game-space")
         element.innerText = gameBoard.board[i];
         element.addEventListener("click", (e)=> {
             let player = gameBoard.whosTurn();
-            player.move(e.target.dataKey);
-            e.target.innerText = player.token;
+            player.move(e.target["data-key"]);
         });
         grid.append(element);
     }
+
+    let undoButton = document.getElementById("actions__undo");
+    undoButton.addEventListener("click", (e)=>{
+        console.log(e.target);
+        gameBoard.undoLastMove();
+        updateGrid();
+        undoButton.classList.add("inactive")
+    });
     
     function updateGrid (){
-        let elements = array.from(grid.querySelectorAll("div"));
-        elements.forEach(()=>{
-            element.innerText = gameBoard[i];
-        });
+        let elements = Array.from(grid.querySelectorAll("div"));
+        for (i=0; i<elements.length; i++){
+            let space=elements[i];
+            let location = space["data-key"];
+            space.innerText = gameBoard.board[location];
+        };
     }
     return {updateGrid};
-}();
+})();
 
 
-const players = function(){
+const players = (function(){
     const _mover = (state) => ({
         move(location){
             gameBoard.board[location] = state.token;
             gameBoard.turn ++;
+            gameBoard.lastMove = location;
+            displayController.updateGrid();
+            let undoButton = document.getElementById("actions__undo");
+            undoButton.classList.remove("inactive");
             gameBoard.checkWin();
         }
     });
@@ -90,7 +111,7 @@ const players = function(){
     let list = [player1, player2];
 
     return {player1, player2, list};
-}();
+})();
 
 
 // console stuff
